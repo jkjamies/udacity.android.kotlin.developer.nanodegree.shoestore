ee936ff7351ee046f82aab0b09792448b6e23302
jkjamies <jkjamies@mtu.edu>
2020-12-30 00:44:26 -0500

    added MIT and Udacity Honor Code for license

--- app/src/main/java/com/udacity/shoestore/screens/listings/ListingsFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/listings/ListingsFragment.kt
@@ -1,8 +1,46 @@
+/*
+ *
+ * PROJECT LICENSE
+ *
+ * This project was submitted by Jason Jamieson as part of the Nanodegree At Udacity.
+ *
+ * As part of Udacity Honor code, your submissions must be your own work, hence
+ * submitting this project as yours will cause you to break the Udacity Honor Code
+ * and the suspension of your account.
+ *
+ * Me, the author of the project, allow you to check the code as a reference, but if
+ * you submit it, it's your own responsibility if you get expelled.
+ *
+ * Copyright (c) 2020 Jason Jamieson
+ *
+ * Besides the above notice, the following license applies and this license notice
+ * must be included in all works derived from this project.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.udacity.shoestore.screens.listings
 
 import android.os.Bundle
 import android.view.*
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -13,6 +51,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.ListingsFragmentBinding
+import com.udacity.shoestore.databinding.ShoeListItemBinding
 import timber.log.Timber
 
 /**
@@ -40,14 +79,10 @@ class ListingsFragment : Fragment() {
         // Observe the shoe list
         viewModel.shoeList.observe(viewLifecycleOwner, Observer { shoes ->
             shoes.forEach {
-                val newView = this.layoutInflater.inflate(R.layout.shoe_list_item, null)
-                newView.findViewById<TextView>(R.id.listItemShoeNameTextView).text = it.name
-                newView.findViewById<TextView>(R.id.listItemShoeCompanyTextView).text = it.company
-                newView.findViewById<TextView>(R.id.listItemShoeSizeTextView).text =
-                    it.size.toString()
-                newView.findViewById<TextView>(R.id.listItemShoeDescriptionTextView).text =
-                    it.description
-                binding.listingsLinearLayout.addView(newView)
+                val newBindingView: ShoeListItemBinding =
+                    DataBindingUtil.inflate(inflater, R.layout.shoe_list_item, container, false)
+                newBindingView.shoeModel = it
+                binding.listingsLinearLayout.addView(newBindingView.root)
             }
         })
 
@@ -57,7 +92,10 @@ class ListingsFragment : Fragment() {
             viewModel.init()
             findNavController().navigate(ListingsFragmentDirections.actionListingsFragmentToDetailsFragment())
         }
+
+        // Set fragment has options menu
         setHasOptionsMenu(true)
+
         return binding.root
     }
 
